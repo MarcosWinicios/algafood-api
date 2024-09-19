a2165ffe7c117c15fda50822f61515e5e6bb851b
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-19 06:40:47 -0300

    test(kitchen): add test class - find kitchen by id

---
+++ src/main/java/com/studies/algafood/jpa/GetKitchenMain.java
@@ -0,0 +1,26 @@
+package com.studies.algafood.jpa;
+
+import com.studies.algafood.AlgafoodApiApplication;
+import com.studies.algafood.domain.model.Kitchen;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class GetKitchenMain {
+    public static void main(String[] args) {
+        /**
+         * Prepara um contexto da aplicação para não executar como uma aplicação Web
+         */
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        KitchenRegister kitchenRegister =  applicationContext.getBean(KitchenRegister.class);
+
+        Kitchen kitchenList =  kitchenRegister.find(1L);
+
+        System.out.println(kitchenList.getName());
+    }
+}
