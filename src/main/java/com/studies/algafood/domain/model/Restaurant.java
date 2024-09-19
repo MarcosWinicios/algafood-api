aa78c8a1752a6eea0356a22e4dcf26d70257dafa
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-19 01:22:05 -0300

    feat: Creating Kitchen and Restaurant Classes in model package with ORM

---
+++ src/main/java/com/studies/algafood/domain/model/Restaurant.java
@@ -0,0 +1,57 @@
+package com.studies.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity(name = "tb_restaurant")
+public class Restaurant {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Column(name = "shipping_fee")
+    private BigDecimal shippingFee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(BigDecimal shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/studies/algafood/domain/model/kitchen.java
@@ -0,0 +1,47 @@
+package com.studies.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_kitchens")
+public class kitchen {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        kitchen kitchen = (kitchen) o;
+        return Objects.equals(id, kitchen.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
