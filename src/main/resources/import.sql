5dca9f1d4e93f8dcf61a67b20cfaa7f92d7f76ff
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-24 03:06:32 -0300

    chore(test-data-load): update initial SQL file - add insertions into tb_restaurant table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO tb_kitchen (name) VALUES ('Tailandesa');
 INSERT INTO tb_kitchen (name) VALUES ('Indiana');
+
+INSERT INTO tb_restaurant(name, shipping_fee) VALUES ('Restaurante do Bob', 5.50);
+INSERT INTO tb_restaurant(name, shipping_fee) VALUES ('Restaurante da Tia Maria', 4.80);
