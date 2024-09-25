a2e7878d73ada73587ff800a3f9460433b4bf165
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-24 22:41:57 -0300

    chore(test-data-load): update test - update test that gets list of restaurants

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_kitchen (name) VALUES ('Tailandesa');
-INSERT INTO tb_kitchen (name) VALUES ('Indiana');
+INSERT INTO tb_kitchen (id, name) VALUES (1, 'Tailandesa');
+INSERT INTO tb_kitchen (id, name) VALUES (2, 'Indiana');
 
-INSERT INTO tb_restaurant(name, shipping_fee) VALUES ('Thai Gourmet', 10.00);
-INSERT INTO tb_restaurant(name, shipping_fee) VALUES ('Thai Delivery', 9.50);
-INSERT INTO tb_restaurant(name, shipping_fee) VALUES ('Tuk Tuk Comida Indiana', 15.00);
+INSERT INTO tb_restaurant(id, name, shipping_fee, kitchen_id) VALUES (1, 'Thai Gourmet', 10.00, 1);
+INSERT INTO tb_restaurant(id, name, shipping_fee, kitchen_id) VALUES (2, 'Thai Delivery', 9.50, 1);
+INSERT INTO tb_restaurant(id, name, shipping_fee, kitchen_id) VALUES (3, 'Tuk Tuk Comida Indiana', 15.00, 2);
