508a2b745b6fe3bc27e6373f298fa1d018a01033
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-24 22:36:02 -0300

    feat(model): update orm - add relationship many to one between classes Restaurant and Kitchen

--- src/main/java/com/studies/algafood/domain/model/Restaurant.java
+++ src/main/java/com/studies/algafood/domain/model/Restaurant.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,6 +28,9 @@ public class Restaurant {
     private Long id;
     private String name;
     private BigDecimal shippingFee;
+
+    @ManyToOne
+    private Kitchen kitchen;
 
     public Restaurant(String name, BigDecimal shippingFee) {
         this.name = name;
