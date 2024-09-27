37caf27128bedf98a0dca791c1161441d75321f7
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-27 02:34:36 -0300

    feat(model): add relationship between State and City

--- src/main/java/com/studies/algafood/domain/model/City.java
+++ src/main/java/com/studies/algafood/domain/model/City.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,6 +28,10 @@ public class City {
 
     @Column(nullable = false)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private State state;
 
     public City(String name){
         this.name = name;
--- src/main/java/com/studies/algafood/domain/model/State.java
+++ src/main/java/com/studies/algafood/domain/model/State.java
@@ -2,14 +2,19 @@ package com.studies.algafood.domain.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
