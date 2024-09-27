e6949eefdbb0ea3651886c0ed4cbddd215eb2cf7
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-26 23:05:51 -0300

    test(State): add class to test CRUD of States

---
+++ src/main/java/com/studies/algafood/jpa/State/ListStatesMain.java
@@ -0,0 +1,39 @@
+package com.studies.algafood.jpa.State;
+
+import com.studies.algafood.AlgafoodApiApplication;
+import com.studies.algafood.domain.model.State;
+import com.studies.algafood.domain.repository.StateRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ListStatesMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        StateRepository allStates =  applicationContext.getBean(StateRepository.class);
+
+        List<State> stateList =  allStates.list();
+        stateList.forEach(System.out::println);
+
+
+        State state = allStates.find(1L);
+        System.out.println(state);
+
+        State newState = allStates.save(new State("Novo state"));
+
+        allStates.list().forEach(System.out::println);
+
+        newState.setName("NOVO NOME");
+
+
+
+    }
+
+
+}
