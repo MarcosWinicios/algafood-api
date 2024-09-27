e9f4b452f917b74d2a25f6be3e963f38a4f23715
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-26 23:59:06 -0300

    feat(City): add City class with ORM mapping

---
+++ src/main/java/com/studies/algafood/domain/model/City.java
@@ -0,0 +1,33 @@
+package com.studies.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_city")
+public class City {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public City(String name){
+        this.name = name;
+    }
+}
