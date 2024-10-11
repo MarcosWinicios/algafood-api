fe8bc0f9684016575ae837034c2cb94b0a3a15d9
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-10 23:39:01 -0300

    feat(permission): add Permission class

---
+++ src/main/java/com/studies/algafood/domain/model/Permission.java
@@ -0,0 +1,38 @@
+package com.studies.algafood.domain.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_permission")
+public class Permission {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    public Permission(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+}
