d65a8abaaf503d40f35334189ee07dbe94eb0c31
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-02 03:43:00 -0300

    feat(kitchen): add a new endpoint to find a unique kitchen by ID

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -5,6 +5,7 @@ import com.studies.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class KitchenController {
     @GetMapping
     public List<Kitchen> list(){
         return kitchenRepository.list();
+    }
+
+    @GetMapping("/{kitchenId}")
+    public Kitchen find(@PathVariable Long kitchenId){
+        return this.kitchenRepository.find(kitchenId);
     }
 
 }
