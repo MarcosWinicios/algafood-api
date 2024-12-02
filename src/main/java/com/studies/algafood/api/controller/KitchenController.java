c0f08d7a92e345aac41c6d426f0a61e208a971cc
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-02 05:31:43 -0300

    feat(kitchen): enable xml format in kitchens endpoints response

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -3,7 +3,6 @@ package com.studies.algafood.api.controller;
 import com.studies.algafood.domain.model.Kitchen;
 import com.studies.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/kitchens", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/kitchens"/*, produces = MediaType.APPLICATION_JSON_VALUE*/)
 public class KitchenController {
 
     @Autowired
