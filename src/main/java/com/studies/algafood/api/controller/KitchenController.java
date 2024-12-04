a3d1263fdd1e9c42d5a6743b0249975185df5017
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-03 23:44:19 -0300

    refactor(kitchen): refactoring KitchenController class

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/kitchens"/*, produces = MediaType.APPLICATION_JSON_VALUE*/)
+@RequestMapping("/kitchens")
 public class KitchenController {
 
     @Autowired
