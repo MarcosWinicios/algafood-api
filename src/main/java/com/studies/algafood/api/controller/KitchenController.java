6b080b51cad8702997a6fbfb16e0ea00bd651512
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-06 19:52:22 -0300

    feat(kitchen): add new method to register news kitchens

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -4,11 +4,15 @@ import com.studies.algafood.api.model.KitchensXmlWrapper;
 import com.studies.algafood.domain.model.Kitchen;
 import com.studies.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -43,6 +47,12 @@ public class KitchenController {
 
 //        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Kitchen add(@RequestBody Kitchen kitchen){
+        return this.kitchenRepository.save(kitchen);
     }
 
 
