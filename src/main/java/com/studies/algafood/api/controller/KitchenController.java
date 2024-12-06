e7ca74b4e4ca3853be2e470132743635078f89b6
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-05 23:48:13 -0300

    refactor(kitchen): remove unused imports

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -4,8 +4,6 @@ import com.studies.algafood.api.model.KitchensXmlWrapper;
 import com.studies.algafood.domain.model.Kitchen;
 import com.studies.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
