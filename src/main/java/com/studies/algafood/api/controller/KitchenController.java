3969c39d68adbbf9aab4d6810e7e10043ff134ee
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-08 22:39:55 -0300

    feat(kitchen): add new method to update kitchens

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -3,6 +3,7 @@ package com.studies.algafood.api.controller;
 import com.studies.algafood.api.model.KitchensXmlWrapper;
 import com.studies.algafood.domain.model.Kitchen;
 import com.studies.algafood.domain.repository.KitchenRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,23 +28,23 @@ public class KitchenController {
     private KitchenRepository kitchenRepository;
 
     @GetMapping
-    public ResponseEntity<List<Kitchen>> list(){
+    public ResponseEntity<List<Kitchen>> list() {
         List<Kitchen> result = this.kitchenRepository.list();
         return ResponseEntity.ok(result);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<KitchensXmlWrapper> listXML(){
+    public ResponseEntity<KitchensXmlWrapper> listXML() {
         List<Kitchen> result = this.kitchenRepository.list();
         KitchensXmlWrapper resultXml = new KitchensXmlWrapper(result);
         return ResponseEntity.ok(resultXml);
     }
 
     @GetMapping("/{kitchenId}")
-    public ResponseEntity<Kitchen> find(@PathVariable Long kitchenId){
+    public ResponseEntity<Kitchen> find(@PathVariable Long kitchenId) {
         Kitchen kitchen = this.kitchenRepository.find(kitchenId);
 
-        if(kitchen != null){
+        if (kitchen != null) {
             return ResponseEntity.ok(kitchen);
         }
 
@@ -52,10 +54,24 @@ public class KitchenController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Kitchen> add(@RequestBody Kitchen kitchen){
+    public ResponseEntity<Kitchen> add(@RequestBody Kitchen kitchen) {
         kitchen = this.kitchenRepository.save(kitchen);
 
         return ResponseEntity.created(URI.create("localhost:8080/kitchens/" + kitchen.getId())).body(kitchen);
+    }
+
+    @PutMapping("/{kitchenId}")
+    public ResponseEntity<Kitchen> update(@PathVariable Long kitchenId, @RequestBody Kitchen kitchen) {
+        Kitchen currentKitchen = this.kitchenRepository.find(kitchenId);
+
+        if (currentKitchen != null) {
+            BeanUtils.copyProperties(kitchen, currentKitchen, "id");
+            currentKitchen = this.kitchenRepository.save(currentKitchen);
+
+            return ResponseEntity.ok(currentKitchen);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 
