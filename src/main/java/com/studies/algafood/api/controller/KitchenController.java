e5570dec87b8a502efb8091b83bb7691a682457f
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-08 22:58:04 -0300

    feat(kitchen): add new method to delete kitchens

--- src/main/java/com/studies/algafood/api/controller/KitchenController.java
+++ src/main/java/com/studies/algafood/api/controller/KitchenController.java
@@ -5,9 +5,11 @@ import com.studies.algafood.domain.model.Kitchen;
 import com.studies.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,5 +76,21 @@ public class KitchenController {
         return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{kitchenId}")
+    public ResponseEntity<Void> delete(@PathVariable Long kitchenId) {
+
+        try {
+            Kitchen kitchen = this.kitchenRepository.find(kitchenId);
+
+            if (kitchen != null) {
+                this.kitchenRepository.remove(kitchen);
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.notFound().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
 
 }
