1682882cd47dbb46642a925692ed49441bfb85d0
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-10 23:44:10 -0300

    feat(kitchen): remove xml wrapper class

--- src/main/java/com/studies/algafood/domain/model/Kitchen.java
+++ src/main/java/com/studies/algafood/domain/model/Kitchen.java
@@ -1,7 +1,6 @@
 package com.studies.algafood.domain.model;
 
 
-import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +10,6 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@JsonRootName("kitchen")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)//gera i equals and hashCode somente se estiver explícito em um atributo
 @Entity
