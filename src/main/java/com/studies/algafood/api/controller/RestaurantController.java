088c0fdee8eb560677cd3aea62754d31327fdf38
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-12 00:33:25 -0300

    feat(restaurant): add exceptions handle in the register restaurants

--- src/main/java/com/studies/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/studies/algafood/api/controller/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.studies.algafood.api.controller;
 
+import com.studies.algafood.domain.exception.EntityNotFoundException;
 import com.studies.algafood.domain.model.Restaurant;
 import com.studies.algafood.domain.repository.RestaurantRepository;
 import com.studies.algafood.domain.service.RestaurantRegisterService;
@@ -11,10 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -45,10 +44,14 @@ public class RestaurantController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Restaurant> save(@RequestBody Restaurant restaurant){
-        restaurant = this.restaurantRegisterService.save(restaurant);
+    public ResponseEntity<?> save(@RequestBody Restaurant restaurant){
 
-        return ResponseEntity.created(URI.create("localhost:8080/restaurants/" + restaurant.getId())).body(restaurant);
+        try {
+            restaurant = this.restaurantRegisterService.save(restaurant);
+            return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
--- src/main/java/com/studies/algafood/domain/service/RestaurantRegisterService.java
+++ src/main/java/com/studies/algafood/domain/service/RestaurantRegisterService.java
@@ -1,6 +1,9 @@
 package com.studies.algafood.domain.service;
 
+import com.studies.algafood.domain.exception.EntityNotFoundException;
+import com.studies.algafood.domain.model.Kitchen;
 import com.studies.algafood.domain.model.Restaurant;
+import com.studies.algafood.domain.repository.KitchenRepository;
 import com.studies.algafood.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +14,20 @@ public class RestaurantRegisterService {
     @Autowired
     private RestaurantRepository restaurantRepository;
 
+    @Autowired
+    private KitchenRepository kitchenRepository;
+
     public Restaurant save(Restaurant restaurant){
+        Long kitchenId = restaurant.getKitchen().getId();
+        Kitchen kitchen = this.kitchenRepository.find(kitchenId);
+
+        if(kitchen == null){
+            throw new EntityNotFoundException(
+                    String.format("There is no kitchen record with code %d", kitchenId)
+            );
+        }
+
+        restaurant.setKitchen(kitchen);
         return this.restaurantRepository.save(restaurant);
     }
 }
