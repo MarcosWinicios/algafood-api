3f14e433703ea2d648a118e74c9723e3a3f7fb18
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-13 01:30:19 -0300

    feat(city): add new endpoint to find city by id

--- src/main/java/com/studies/algafood/api/controller/CityController.java
+++ src/main/java/com/studies/algafood/api/controller/CityController.java
@@ -5,6 +5,7 @@ import com.studies.algafood.domain.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,16 @@ public class CityController {
         List<City> result = cityRepository.list();
 
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{cityId}")
+    public ResponseEntity<City> find(@PathVariable Long cityId){
+        City city = this.cityRepository.find(cityId);
+
+        if(city != null){
+            return ResponseEntity.ok(city);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
