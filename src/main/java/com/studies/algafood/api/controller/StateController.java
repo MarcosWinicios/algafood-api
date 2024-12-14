31c8a07487500f6534b359ef22cce8cd6930401a
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-13 23:06:11 -0300

    feat(state): add new endpoint to find states by id

--- src/main/java/com/studies/algafood/api/controller/StateController.java
+++ src/main/java/com/studies/algafood/api/controller/StateController.java
@@ -5,6 +5,7 @@ import com.studies.algafood.domain.repository.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,15 @@ public class StateController {
     public ResponseEntity<List<State>> list(){
         List<State> result = this.stateRepository.list();
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{stateId}")
+    public ResponseEntity<State> find(@PathVariable Long stateId){
+        State state = this.stateRepository.find(stateId);
+
+        if (state != null){
+            return ResponseEntity.ok(state);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
