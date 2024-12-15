44a9efd0cce0f1125ee0febf7f44c9dc1c677d9c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-15 18:12:53 -0300

    feat(restaurant): add new endpoint to make partial updates to the restaurants object properties, using the Patch method and reflection

--- src/main/java/com/studies/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/studies/algafood/api/controller/RestaurantController.java
@@ -1,15 +1,17 @@
 package com.studies.algafood.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studies.algafood.domain.exception.EntityNotFoundException;
 import com.studies.algafood.domain.model.Restaurant;
 import com.studies.algafood.domain.repository.RestaurantRepository;
 import com.studies.algafood.domain.service.RestaurantRegisterService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,7 +19,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.*;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/restaurants")
@@ -74,6 +80,34 @@ public class RestaurantController {
         }catch (EntityNotFoundException e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @PatchMapping("/{restaurantId}")
+    public ResponseEntity<?> partialUpdate(@PathVariable Long restaurantId, @RequestBody Map<String, Object> fields){
+        Restaurant currentRestaurant = this.restaurantRepository.find(restaurantId);
+
+        if(restaurantId == null){
+            return ResponseEntity.notFound().build();
+        }
+        merge(fields, currentRestaurant);
+
+        return update(restaurantId, currentRestaurant);
+    }
+
+    private void merge(Map<String, Object> dataSource, Restaurant restaurantTarget) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurant restaurantSource = objectMapper.convertValue(dataSource, Restaurant.class); //Usando o objectMapper para converter valores de tipos desconhecidos
+
+        dataSource.forEach((fieldName, fieldValue) -> {
+            Field field = ReflectionUtils.findField(Restaurant.class, fieldName); //findField() Retorna uma instância de uma propriedade
+            field.setAccessible(true);
+
+            Object newValue =  ReflectionUtils.getField(field, restaurantSource); //getFiedl() Obtem o valor de uma propriedade
+
+//            System.out.println(fieldName + " = " + fieldValue + " = " + newValue);
+
+            ReflectionUtils.setField(field, restaurantTarget, newValue);
+        });
     }
 
 }
