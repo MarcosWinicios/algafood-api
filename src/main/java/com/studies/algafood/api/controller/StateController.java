83b1ec36839b4a2a5a9639e86a79f642f4d5fb83
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-14 21:41:54 -0300

    feat(state): add new endpoint to update states

--- src/main/java/com/studies/algafood/api/controller/StateController.java
+++ src/main/java/com/studies/algafood/api/controller/StateController.java
@@ -5,6 +5,7 @@ import com.studies.algafood.domain.exception.EntityNotFoundException;
 import com.studies.algafood.domain.model.State;
 import com.studies.algafood.domain.repository.StateRepository;
 import com.studies.algafood.domain.service.StateRegisterService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,16 +32,16 @@ public class StateController {
     private StateRegisterService stateRegisterService;
 
     @GetMapping
-    public ResponseEntity<List<State>> list(){
+    public ResponseEntity<List<State>> list() {
         List<State> result = this.stateRepository.list();
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{stateId}")
-    public ResponseEntity<State> find(@PathVariable Long stateId){
+    public ResponseEntity<State> find(@PathVariable Long stateId) {
         State state = this.stateRepository.find(stateId);
 
-        if (state != null){
+        if (state != null) {
             return ResponseEntity.ok(state);
         }
         return ResponseEntity.notFound().build();
@@ -47,20 +49,33 @@ public class StateController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<State> save(@RequestBody State state){
-        state =  this.stateRegisterService.save(state);
+    public ResponseEntity<State> save(@RequestBody State state) {
+        state = this.stateRegisterService.save(state);
 
         return ResponseEntity.ok(state);
     }
 
+    @PutMapping("/{stateId}")
+    public ResponseEntity<?> update(@PathVariable Long stateId, @RequestBody State state) {
+        State currentState = this.stateRepository.find(stateId);
+
+        if (currentState != null) {
+            BeanUtils.copyProperties(state, currentState, "id");
+            currentState = this.stateRegisterService.save(currentState);
+            return ResponseEntity.ok(currentState);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
     @DeleteMapping("/{stateId}")
-    public ResponseEntity<Void> delete(@PathVariable Long stateId){
+    public ResponseEntity<Void> delete(@PathVariable Long stateId) {
         try {
             this.stateRegisterService.remove(stateId);
             return ResponseEntity.noContent().build();
-        }catch (EntityNotFoundException e){
+        } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
-        }catch (EntityInUseException e){
+        } catch (EntityInUseException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
