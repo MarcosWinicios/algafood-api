e1fd5d2f6e8427eb627e72ff8291619712f29ada
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-15 21:27:35 -0300

    refactor(restaurant): refactor restaurant controller

--- src/main/java/com/studies/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/studies/algafood/api/controller/RestaurantController.java
@@ -19,9 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.*;
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
