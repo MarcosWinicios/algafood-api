df9c7a525b20b21e2d019f89a0748e88d00483bc
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-24 18:41:09 -0300

    feat(chore): add printing of the beans name list and beans quantity

--- src/main/java/com/studies/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/studies/algafood/AlgafoodApiApplication.java
@@ -2,12 +2,25 @@ package com.studies.algafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgafoodApiApplication.class, args);
+		/*
+		ApplicationContext applicationContext =  SpringApplication.run(AlgafoodApiApplication.class, args);
+
+		String[] allBeansNames = applicationContext.getBeanDefinitionNames();
+		Arrays.stream(allBeansNames).forEach(System.out::println);
+
+		System.out.println(allBeansNames.length);
+		System.out.println(applicationContext.getBeanDefinitionCount());
+		*/
+
 	}
 
 }
