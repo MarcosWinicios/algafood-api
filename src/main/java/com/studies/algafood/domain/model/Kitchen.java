ad9833d27b63b310c0b33f10979b39df9458975a
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-29 22:57:56 -0300

    feat(kitchen): Add bidirectional relationship between restaurant and kitchen entities

--- src/main/java/com/studies/algafood/domain/model/Kitchen.java
+++ src/main/java/com/studies/algafood/domain/model/Kitchen.java
@@ -1,14 +1,19 @@
 package com.studies.algafood.domain.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)//gera i equals and hashCode somente se estiver explícito em um atributo
@@ -23,4 +28,9 @@ public class Kitchen {
 
     @Column(nullable = false)
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "kitchen")
+    private List<Restaurant> restaurants = new ArrayList<>();
+
 }
