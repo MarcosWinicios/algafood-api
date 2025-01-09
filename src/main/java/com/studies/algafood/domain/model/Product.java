bdd26c15b0471cb1554ce6ae47a7960c60726a7a
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-01-09 00:50:40 -0300

    feat(product): Add new entity Product

---
+++ src/main/java/com/studies/algafood/domain/model/Product.java
@@ -0,0 +1,40 @@
+package com.studies.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private Boolean isActive;
+
+}
