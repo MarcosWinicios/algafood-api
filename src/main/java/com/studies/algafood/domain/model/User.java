93555c0110bdf1901dcb1c99ec66f28eb50ea851
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-01-09 22:37:25 -0300

    feat(user): Add relationship ManyToOne between from user to Restaurant Entities

--- src/main/java/com/studies/algafood/domain/model/User.java
+++ src/main/java/com/studies/algafood/domain/model/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -35,4 +37,7 @@ public class User {
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
 }
