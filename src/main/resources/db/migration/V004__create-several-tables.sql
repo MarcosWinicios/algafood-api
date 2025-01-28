faf920e406eea53b0b30cf2bad9b6a7dd458df42
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-01-28 01:15:12 -0300

    fix(migration): Fix migration v004 sql script

--- src/main/resources/db/migration/V004__create-several-tables.sql
+++ src/main/resources/db/migration/V004__create-several-tables.sql
@@ -1,122 +1,120 @@
 
-    create table tb_group (
-        id bigint not null auto_increment,
-        name varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_group (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(60) NOT NULL,
+
+        PRIMARY KEY (id)
     ) engine=InnoDB;
 
-    create table tb_group_permission (
-        group_id bigint not null,
-        permission_id bigint not null
+    CREATE TABLE tb_group_permission (
+        group_id BIGINT NOT NULL,
+        permission_id BIGINT NOT NULL
     ) engine=InnoDB;
 
-    create table tb_kitchen (
-        id bigint not null auto_increment,
-        name varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_payment_method (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        description VARCHAR(60) NOT NULL,
+
+        PRIMARY KEY (id)
     ) engine=InnoDB;
 
-    create table tb_payment_method (
-        id bigint not null auto_increment,
-        description varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_permission (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+
+        PRIMARY KEY (id)
     ) engine=InnoDB;
 
-    create table tb_permission (
-        id bigint not null auto_increment,
-        description varchar(255) not null,
-        name varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_product (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(80) NOT NULL,
+        description TEXT NOT NULL,
+        price DECIMAL(10,2) NOT NULL,
+        is_active TINYINT(1) NOT NULL,
+        restaurant_id BIGINT NOT NULL,
+
+        PRIMARY KEY (id)
     ) engine=InnoDB;
 
-    create table tb_product (
-        is_active bit not null,
-        price decimal(38,2) not null,
-        id bigint not null auto_increment,
-        restaurant_id bigint not null,
-        description varchar(255) not null,
-        name varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_restaurant (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        shipping_fee DECIMAL(10,2) NOT NULL,
+        updated_at DATETIME NOT NULL,
+        created_at DATETIME NOT NULL,
+        address_city_id BIGINT,
+        address_postal_code VARCHAR(9),
+        address_public_place VARCHAR(255),
+        address_number VARCHAR(255),
+        address_complement VARCHAR(255),
+        address_neighborhood VARCHAR(255),
+        kitchen_id BIGINT NOT NULL,
+
+        PRIMARY KEY (id)
     ) engine=InnoDB;
 
-    create table tb_restaurant (
-        is_active bit not null,
-        is_open bit not null,
-        shipping_fee decimal(38,2) not null,
-        address_city_id bigint,
-        created_at datetime not null,
-        id bigint not null auto_increment,
-        kitchen_id bigint not null,
-        updated_at datetime not null,
-        address_complement varchar(255),
-        address_neighborhood varchar(255),
-        address_number varchar(255),
-        address_postal_code varchar(255),
-        address_public_place varchar(255),
-        name varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_restaurant_payment_method (
+        payment_method_id BIGINT NOT NULL,
+        restaurant_id BIGINT NOT NULL
     ) engine=InnoDB;
 
-    create table tb_restaurant_payment_method (
-        payment_method_id bigint not null,
-        restaurant_id bigint not null
+    CREATE TABLE tb_user (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(80) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at DATETIME NOT NULL,
+
+        PRIMARY KEY (id)
     ) engine=InnoDB;
 
-    create table tb_user (
-        created_at datetime not null,
-        id bigint not null auto_increment,
-        email varchar(255) not null,
-        name varchar(255) not null,
-        password varchar(255) not null,
-        primary key (id)
+    CREATE TABLE tb_user_group (
+        group_id BIGINT NOT NULL,
+        user_id BIGINT NOT NULL
     ) engine=InnoDB;
 
-    create table tb_user_group (
-        group_id bigint not null,
-        user_id bigint not null
-    ) engine=InnoDB;
+    ALTER TABLE tb_group_permission
+       ADD CONSTRAINT fk_group_permission_permission
+       FOREIGN KEY (permission_id)
+       REFERENCES tb_permission (id);
 
-    alter table tb_group_permission
-       add constraint fk_group_permission_permission
-       foreign key (permission_id)
-       references tb_permission (id);
+    ALTER TABLE tb_group_permission
+       ADD CONSTRAINT fk_group_permission_group
+       FOREIGN KEY (group_id)
+       REFERENCES tb_group (id);
 
-    alter table tb_group_permission
-       add constraint fk_group_permission_group
-       foreign key (group_id)
-       references tb_group (id);
+    ALTER TABLE tb_product
+       ADD CONSTRAINT fk_product_restaurant
+       FOREIGN KEY (restaurant_id)
+       REFERENCES tb_restaurant (id);
 
-    alter table tb_product
-       add constraint fk_product_restaurant
-       foreign key (restaurant_id)
-       references tb_restaurant (id);
+    ALTER TABLE tb_restaurant
+       ADD CONSTRAINT fk_restaurant_city
+       FOREIGN KEY (address_city_id)
+       REFERENCES tb_city (id);
 
-    alter table tb_restaurant
-       add constraint fk_restaurant_city
-       foreign key (address_city_id)
-       references tb_city (id);
+    ALTER TABLE tb_restaurant
+       ADD CONSTRAINT fk_restaurant_kitchen
+       FOREIGN KEY (kitchen_id)
+       REFERENCES tb_kitchen (id);
 
-    alter table tb_restaurant
-       add constraint fk_restaurant_kitchen
-       foreign key (kitchen_id)
-       references tb_kitchen (id);
+    ALTER TABLE tb_restaurant_payment_method
+       ADD CONSTRAINT fk_restaurant_payment_method_payment_method
+       FOREIGN KEY (payment_method_id)
+       REFERENCES tb_payment_method (id);
 
-    alter table tb_restaurant_payment_method
-       add constraint fk_restaurant_payment_method_payment_method
-       foreign key (payment_method_id)
-       references tb_payment_method (id);
+    ALTER TABLE tb_restaurant_payment_method
+       ADD CONSTRAINT fk_restaurant_payment_method_restaurant
+       FOREIGN KEY (restaurant_id)
+       REFERENCES tb_restaurant (id);
 
-    alter table tb_restaurant_payment_method
-       add constraint fk_restaurant_payment_method_restaurant
-       foreign key (restaurant_id)
-       references tb_restaurant (id);
+    ALTER TABLE tb_user_group
+       ADD CONSTRAINT fk_user_group_group
+       FOREIGN KEY (group_id)
+       REFERENCES tb_group (id);
 
-    alter table tb_user_group
-       add constraint fk_user_group_group
-       foreign key (group_id)
-       references tb_group (id);
-
-    alter table tb_user_group
-       add constraint fk_user_group_user
-       foreign key (user_id)
-       references tb_user (id);
+    ALTER TABLE tb_user_group
+       ADD CONSTRAINT fk_user_group_user
+       FOREIGN KEY (user_id)
+       REFERENCES tb_user (id);
