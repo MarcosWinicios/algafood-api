466c2d9f3e7ed2767468b816d0862583ee16529f
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-02-02 21:28:32 -0300

    feat(order): Add Order entity

---
+++ src/main/java/com/studies/algafood/domain/model/Order.java
@@ -0,0 +1,49 @@
+package com.studies.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal subTotal;
+
+    @Column(nullable = false)
+    private BigDecimal shippingFee;
+
+    @Column(nullable = false)
+    private BigDecimal totalValue;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime confirmationDate;
+
+    private LocalDateTime deliveryDate;
+
+    private LocalDateTime cancellationDate;
+}
