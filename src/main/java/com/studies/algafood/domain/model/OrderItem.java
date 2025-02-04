8b1fee493679c80c68380639aad95021c791e639
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-02-04 01:52:42 -0300

    feat(order-item): Add new Entity OrderItem

---
+++ src/main/java/com/studies/algafood/domain/model/OrderItem.java
@@ -0,0 +1,46 @@
+package com.studies.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private String observation;
+
+    @OneToMany
+    @JoinColumn(nullable = false, name = "product_id")
+    private Product product;
+}
