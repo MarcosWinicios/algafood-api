7bc4e7c4e7e0741c2611d15604c59187c21aaff6
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-02-05 01:39:17 -0300

    refactor(order): Refactor OrderItem entity

--- src/main/java/com/studies/algafood/domain/model/OrderItem.java
+++ src/main/java/com/studies/algafood/domain/model/OrderItem.java
@@ -29,22 +29,23 @@ public class OrderItem {
     private Long id;
 
     @Column(nullable = false)
-    private Integer quantity;
-
-    @Column(nullable = false)
     private BigDecimal unitPrice;
 
     @Column(nullable = false)
     private BigDecimal totalPrice;
 
     @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
     private String observation;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    @ManyToOne
-    @JoinColumn(name = "order_id", nullable = false)
-    private Order order;
 }
